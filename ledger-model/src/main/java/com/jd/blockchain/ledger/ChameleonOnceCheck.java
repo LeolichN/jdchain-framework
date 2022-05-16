c14152b2e925e04b667aabd66e165575e86a602a
曹帅 <43286605+LeolichN@users.noreply.github.com>
2022-05-16 12:39:58 +0800

    move interface implementation to ledger model

---
+++ ledger-model/src/main/java/com/jd/blockchain/ledger/ChameleonOnceCheck.java
@@ -0,0 +1,20 @@
+package com.jd.blockchain.ledger;
+
+import com.jd.blockchain.transaction.DataAccountChameleonOnceCheck;
+import utils.crypto.chameleon.ChameleonUtils;
+
+import java.math.BigInteger;
+
+/**
+ * @author tsao
+ * @version 0.1.0
+ * @create 2022-05-15 22:23
+ * @since 0.1.0
+ **/
+public class ChameleonOnceCheck implements DataAccountChameleonOnceCheck {
+
+    @Override
+    public BytesValue hashDataOnce(byte[] data, byte[] pubKey) {
+        return TypedValue.fromBytes(ChameleonUtils.sign(data, BigInteger.ONE,pubKey));
+    }
+}
